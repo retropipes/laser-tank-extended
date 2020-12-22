89acb3eadee2c58852c75d0eb7bacb7b60395229
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-12-22 08:05:56 -0500

    Get proof of concept build working.

---
+++ LaserTank-Extended/src/Main.hx
@@ -0,0 +1,9 @@
+class Main extends hxd.App {
+    override function init() {
+        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+        tf.text = "LaserTank Extended POC";
+    }
+    static function main() {
+        new Main();
+    }
+}
