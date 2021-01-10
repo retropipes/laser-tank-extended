f2c40efd527754e25b97cde9f1f4e52ea8d90c69
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-01-10 10:57:45 -0500

    Make UI images general.

--- LaserTank-Extended/src/Main.hx
+++ LaserTank-Extended/src/Main.hx
@@ -1,5 +1,6 @@
 class Main extends hxd.App {
     override function init() {
+        super.init();
         var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
         tf.text = "LaserTank Extended POC";
     }
